a97d462d1651323b372adf9d9151a945a692ba01
Peter Donald <peter@realityforge.org>
2019-12-24 15:28:55 +1100

    Add placeholder processor

---
+++ processor/src/main/java/sting/processor/StingProcessor.java
@@ -0,0 +1,58 @@
+package sting.processor;
+
+import com.squareup.javapoet.TypeSpec;
+import java.io.IOException;
+import java.util.Collection;
+import java.util.Collections;
+import javax.annotation.Nonnull;
+import javax.annotation.processing.RoundEnvironment;
+import javax.annotation.processing.SupportedAnnotationTypes;
+import javax.annotation.processing.SupportedOptions;
+import javax.annotation.processing.SupportedSourceVersion;
+import javax.lang.model.SourceVersion;
+import javax.lang.model.element.TypeElement;
+import org.realityforge.proton.AbstractStandardProcessor;
+import org.realityforge.proton.GeneratorUtil;
+import org.realityforge.proton.ProcessorException;
+
+/**
+ * Annotation processor that analyzes sting annotated source and generates dependency injection container.
+ */
+@SupportedAnnotationTypes( { "sting.*" } )
+@SupportedSourceVersion( SourceVersion.RELEASE_8 )
+@SupportedOptions( { "sting.defer.unresolved", "sting.defer.errors" } )
+public final class StingProcessor
+  extends AbstractStandardProcessor
+{
+  @Nonnull
+  @Override
+  protected String getIssueTrackerURL()
+  {
+    return "https://github.com/realityforge/sting/issues";
+  }
+
+  @Nonnull
+  @Override
+  protected String getOptionPrefix()
+  {
+    return "sting";
+  }
+
+  @Nonnull
+  @Override
+  protected Collection<TypeElement> getTypeElementsToProcess( @Nonnull final RoundEnvironment roundEnvironment )
+  {
+    return Collections.emptyList();
+  }
+
+  protected void process( @Nonnull final TypeElement element )
+    throws IOException, ProcessorException
+  {
+  }
+
+  private void emitTypeSpec( @Nonnull final String packageName, @Nonnull final TypeSpec typeSpec )
+    throws IOException
+  {
+    GeneratorUtil.emitJavaType( packageName, typeSpec, processingEnv.getFiler() );
+  }
+}
