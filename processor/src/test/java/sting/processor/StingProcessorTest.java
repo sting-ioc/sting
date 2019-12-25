e1e6a5d09f1eb69c94cd3f66487e47a34239a1bf
Peter Donald <peter@realityforge.org>
2019-12-25 19:02:16 +1100

    Add placeholder test

---
+++ processor/src/test/java/sting/processor/StingProcessorTest.java
@@ -0,0 +1,54 @@
+package sting.processor;
+
+import javax.annotation.Nonnull;
+import javax.annotation.processing.Processor;
+import org.realityforge.proton.qa.AbstractProcessorTest;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+public final class StingProcessorTest
+  extends AbstractProcessorTest
+{
+  @DataProvider( name = "successfulCompiles" )
+  public Object[][] successfulCompiles()
+  {
+    return new Object[][]
+      {
+      };
+  }
+
+  @Test( dataProvider = "successfulCompiles" )
+  public void processSuccessfulCompile( @Nonnull final String classname )
+    throws Exception
+  {
+    assertSuccessfulCompile( classname );
+  }
+
+  @DataProvider( name = "failedCompiles" )
+  public Object[][] failedCompiles()
+  {
+    return new Object[][]
+      {
+      };
+  }
+
+  @Test( dataProvider = "failedCompiles" )
+  public void processFailedCompile( @Nonnull final String classname, @Nonnull final String errorMessageFragment )
+  {
+    assertFailedCompile( classname, errorMessageFragment );
+  }
+
+  @Nonnull
+  @Override
+  protected Processor processor()
+  {
+    return new StingProcessor();
+  }
+
+  @Nonnull
+  @Override
+  protected String getOptionPrefix()
+  {
+    return "sting";
+  }
+}
