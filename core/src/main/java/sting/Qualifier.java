06eca8b078e89fe7795c6adca7130bcdd63bcce0
Peter Donald <peter@realityforge.org>
2019-12-29 15:45:30 +1100

    Improve the documentation somewhat

--- core/src/main/java/sting/Qualifier.java
+++ core/src/main/java/sting/Qualifier.java
@@ -7,27 +7,22 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- * Identifies qualifier annotations. Anyone can define a new qualifier. A
- * qualifier annotation:
- *
- * <ul>
- *   <li>is annotated with {@code @Qualifier}, {@code @Retention(RUNTIME)},
- *      and typically {@code @Documented}.</li>
- *   <li>can have attributes.</li>
- *   <li>may be part of the public API, much like the dependency type, but
- *      unlike implementation types which needn't be part of the public
- *      API.</li>
- * </ul>
+ * Identifies qualifier annotations.
+ * A qualifier annotation is combined with the type of the dependency to restrict which values can satisfy the
+ * dependency. A qualifier annotation is annotated with {@code @Qualifier} and
+ * {@link Retention @Retention(RetentionPolicy.RUNTIME)}. The qualifier annotation is typically annotated with
+ * {@link Documented @Documented} as the dependency is part of the public API of the type. A qualifier annotation
+ * may also have attributes.
  *
  * <p>For example:
  *
  * <pre>
- *   &#064;java.lang.annotation.Documented
- *   &#064;java.lang.annotation.Retention(RetentionPolicy.RUNTIME)
- *   &#064;javax.inject.Qualifier
- *   public @interface Leather {
- *     Color color() default Color.TAN;
- *     public enum Color { RED, BLACK, TAN }
+ *   &#064;Documented
+ *   &#064;Retention(RetentionPolicy.RUNTIME)
+ *   &#064;Qualifier
+ *   public @interface Plugin {
+ *     Phase phase() default Phase.IN;
+ *     public enum Phase { PRE, IN, POST }
  *   }</pre>
  *
  * @see Named @Named
