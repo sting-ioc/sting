52ff88c77cc820b7d5edbd210dffb513b5e82bcf
Peter Donald <peter@realityforge.org>
2020-01-13 08:59:53 +1100

    Remove unnecessary map for types published

--- processor/src/main/java/sting/processor/Registry.java
+++ processor/src/main/java/sting/processor/Registry.java
@@ -2,12 +2,10 @@ package sting.processor;
 
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
-import javax.lang.model.type.TypeMirror;
 
 final class Registry
 {
@@ -26,11 +24,6 @@ final class Registry
    */
   @Nonnull
   private final List<InjectorDescriptor> _injectors = new ArrayList<>();
-  /**
-   * The published types of bindings that have been registered.
-   */
-  @Nonnull
-  private final Map<Coordinate, List<Binding>> _publishedTypes = new LinkedHashMap<>();
 
   /**
    * Register the binding in the local cache.
@@ -40,11 +33,6 @@ final class Registry
   void registerBinding( @Nonnull final Binding binding )
   {
     _bindings.add( binding );
-    for ( final TypeMirror publishedType : binding.getTypes() )
-    {
-      final Coordinate key = new Coordinate( binding.getQualifier(), publishedType );
-      _publishedTypes.computeIfAbsent( key, c -> new ArrayList<>() ).add( binding );
-    }
   }
 
   /**
@@ -89,6 +77,5 @@ final class Registry
     _bindings.clear();
     _fragments.clear();
     _injectors.clear();
-    _publishedTypes.clear();
   }
 }
