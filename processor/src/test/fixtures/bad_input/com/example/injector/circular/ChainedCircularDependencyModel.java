d926653b37cbfd6219797a35035bc3b5a301a245
Peter Donald <peter@realityforge.org>
2020-01-22 10:16:55 +1100

    Combine tests and make sure we traverse along a nullable provides edge

--- processor/src/test/fixtures/bad_input/com/example/injector/circular/ChainedCircularDependencyModel.java
+++ processor/src/test/fixtures/bad_input/com/example/injector/circular/ChainedCircularDependencyModel.java
@@ -1,16 +1,22 @@
 package com.example.injector.circular;
 
+import javax.annotation.Nullable;
+import sting.Fragment;
 import sting.Injectable;
 import sting.Injector;
 
-@Injector
+@Injector( includes = { ChainedCircularDependencyModel.MyFragment1.class,
+                        ChainedCircularDependencyModel.MyFragment2.class } )
 abstract class ChainedCircularDependencyModel
 {
   abstract MyModel1 getMyModel1();
 
   abstract MyModel2 getMyModel2();
 
+  @Nullable
   abstract MyModel3 getMyModel3();
+
+  abstract MyModel4 getMyModel4();
 
   @Injectable
   static class MyModel1
@@ -20,18 +26,44 @@ abstract class ChainedCircularDependencyModel
     }
   }
 
-  @Injectable
+  @Fragment
+  public interface MyFragment1
+  {
+    default MyModel2 provideMyModel2( @Nullable MyModel3 model )
+    {
+      return new MyModel2( model );
+    }
+  }
+
   static class MyModel2
   {
-    MyModel2( MyModel3 model )
+    MyModel2( @Nullable MyModel3 model )
+    {
+    }
+  }
+
+  @Fragment
+  public interface MyFragment2
+  {
+    // Nullable provides
+    @Nullable
+    default MyModel3 provideMyModel3( MyModel4 model )
+    {
+      return new MyModel3( model );
+    }
+  }
+
+  static class MyModel3
+  {
+    MyModel3( MyModel4 model )
     {
     }
   }
 
   @Injectable
-  static class MyModel3
+  static class MyModel4
   {
-    MyModel3( MyModel1 model )
+    MyModel4( MyModel1 model )
     {
     }
   }
