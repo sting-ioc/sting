37b8275df0cac278eaa9f6942610b308d3ae0965
Peter Donald <peter@realityforge.org>
2020-01-27 15:26:06 +1100

    Add first integration test

---
+++ integration-tests/src/test/java/sting/integration/AbstractIntegrationTest.java
@@ -0,0 +1,44 @@
+package sting.integration;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import javax.annotation.Nonnull;
+import org.testng.annotations.BeforeMethod;
+import static org.testng.Assert.*;
+
+public abstract class AbstractIntegrationTest
+{
+  @Nonnull
+  private static final List<String> c_trace = new ArrayList<>();
+
+  public static abstract class BaseModel
+  {
+    BaseModel( final Object... args )
+    {
+      c_trace.add( toString() + Arrays.asList( args ) );
+    }
+
+    @Override
+    public String toString()
+    {
+      return getClass().getSimpleName();
+    }
+  }
+
+  @BeforeMethod
+  protected void preTest()
+  {
+    clearTrace();
+  }
+
+  protected final void clearTrace()
+  {
+    c_trace.clear();
+  }
+
+  protected final void assertCreateTrace( @Nonnull final String trace )
+  {
+    assertEquals( String.join( " ", c_trace ), trace );
+  }
+}
---
+++ integration-tests/src/test/java/sting/integration/BasicIntegrationTest.java
@@ -0,0 +1,97 @@
+package sting.integration;
+
+import javax.annotation.Nonnull;
+import org.testng.annotations.Test;
+import sting.Fragment;
+import sting.Injectable;
+import sting.Injector;
+import static org.testng.Assert.*;
+
+public final class BasicIntegrationTest
+  extends AbstractIntegrationTest
+{
+  @Injectable
+  public static class Model1
+    extends BaseModel
+  {
+  }
+
+  @Injectable
+  public static class Model2
+    extends BaseModel
+  {
+    Model2( @Nonnull final Model1 model1 )
+    {
+      super( model1 );
+    }
+  }
+
+  public static class Model3
+    extends BaseModel
+  {
+    Model3( @Nonnull final Model1 model1, @Nonnull final Model2 model2 )
+    {
+      super( model1, model2 );
+    }
+  }
+
+  @Fragment
+  public interface MyFragment
+  {
+    default Model3 provideModel3( @Nonnull final Model1 model1, @Nonnull final Model2 model2 )
+    {
+      return new Model3( model1, model2 );
+    }
+  }
+
+  @Injector( includes = MyFragment.class )
+  public interface MyInjector
+  {
+    @Nonnull
+    static MyInjector create()
+    {
+      return new BasicIntegrationTest_Sting_MyInjector();
+    }
+
+    Model2 getModel2();
+
+    Model3 getModel3();
+  }
+
+  @Test
+  public void scenario()
+  {
+    clearTrace();
+    final MyInjector injector = MyInjector.create();
+
+    assertCreateTrace( "" );
+
+    final Model2 model2 = injector.getModel2();
+    assertNotNull( model2 );
+
+    assertCreateTrace( "Model1[] Model2[Model1]" );
+
+    final Model3 model3 = injector.getModel3();
+    assertNotNull( model3 );
+
+    assertCreateTrace( "Model1[] Model2[Model1] Model3[Model1, Model2]" );
+
+    final Model3 model3b = injector.getModel3();
+    assertNotNull( model3b );
+    assertSame( model3b, model3 );
+
+    assertCreateTrace( "Model1[] Model2[Model1] Model3[Model1, Model2]" );
+  }
+
+  @Test
+  public void scenario_onlyAccessModel3()
+  {
+    clearTrace();
+    final MyInjector injector = MyInjector.create();
+    final Model3 model3 = injector.getModel3();
+    assertNotNull( model3 );
+
+    final String expected = "Model1[] Model2[Model1] Model3[Model1, Model2]";
+    assertCreateTrace( expected );
+  }
+}
