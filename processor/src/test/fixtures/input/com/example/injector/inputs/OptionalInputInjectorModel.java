c41a1914a052ef43e8f45cd1308bf23558d3d0d1
Peter Donald <peter@realityforge.org>
2020-02-09 14:55:21 +1100

    Correct example

--- processor/src/test/fixtures/input/com/example/injector/inputs/OptionalInputInjectorModel.java
+++ processor/src/test/fixtures/input/com/example/injector/inputs/OptionalInputInjectorModel.java
@@ -1,9 +1,11 @@
 package com.example.injector.inputs;
 
+import javax.annotation.Nullable;
 import sting.Injector;
 
 @Injector( inputs = @Injector.Input( type = Runnable.class, optional = true ) )
 interface OptionalInputInjectorModel
 {
+  @Nullable
   Runnable getRunnable();
 }
