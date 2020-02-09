9834e657811e5b2132bc56bdc2da2db53de4d60b
Peter Donald <peter@realityforge.org>
2020-02-09 14:42:06 +1100

    Stop emitting explicit field for providesMethod as encodinded into id that is now immutable

--- processor/src/main/java/sting/processor/Binding.java
+++ processor/src/main/java/sting/processor/Binding.java
@@ -112,10 +112,6 @@ final class Binding
   void write( @Nonnull final JsonGenerator g )
   {
     g.write( "id", _id );
-    if ( Kind.INJECTABLE != _kind )
-    {
-      g.write( "providesMethod", _element.getSimpleName().toString() );
-    }
     if ( Kind.NULLABLE_PROVIDES == _kind )
     {
       g.write( "nullable", true );
