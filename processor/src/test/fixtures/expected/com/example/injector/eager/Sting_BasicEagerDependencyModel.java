3d1a07a50dc9138bd0e128e49a73132ddf028674
Peter Donald <peter@realityforge.org>
2020-02-13 23:24:32 +1100

    Regenerate fixtures after dependency order changed

--- processor/src/test/fixtures/expected/com/example/injector/eager/Sting_BasicEagerDependencyModel.java
+++ processor/src/test/fixtures/expected/com/example/injector/eager/Sting_BasicEagerDependencyModel.java
@@ -7,14 +7,14 @@ import javax.annotation.Nullable;
 
 @Generated("sting.processor.StingProcessor")
 final class Sting_BasicEagerDependencyModel implements BasicEagerDependencyModel {
-  @Nullable
-  private Object node1;
+  @Nonnull
+  private final Object node1;
 
   @Nullable
   private Object node2;
 
-  @Nonnull
-  private final Object node3;
+  @Nullable
+  private Object node3;
 
   @Nonnull
   private final Object node4;
@@ -29,29 +29,29 @@ final class Sting_BasicEagerDependencyModel implements BasicEagerDependencyModel
   private final Object node7;
 
   Sting_BasicEagerDependencyModel() {
-    node3 = Objects.requireNonNull( BasicEagerDependencyModel_Sting_MyModel2.create() );
+    node1 = Objects.requireNonNull( BasicEagerDependencyModel_Sting_MyModel2.create() );
     node4 = Objects.requireNonNull( BasicEagerDependencyModel_Sting_MyModel1.create() );
-    node5 = Objects.requireNonNull( BasicEagerDependencyModel_Sting_MyModel5.create(node3, () -> node2(), node4) );
-    node6 = Objects.requireNonNull( BasicEagerDependencyModel_Sting_MyModel4.create(node3, () -> node2(), () -> node4) );
+    node5 = Objects.requireNonNull( BasicEagerDependencyModel_Sting_MyModel5.create(node1, () -> node3(), node4) );
+    node6 = Objects.requireNonNull( BasicEagerDependencyModel_Sting_MyModel4.create(node1, () -> node3(), () -> node4) );
     node7 = Objects.requireNonNull( BasicEagerDependencyModel_Sting_MyModel6.create(node6, node5) );
-  }
-
-  @Nonnull
-  private Object node1() {
-    if ( null == node1 ) {
-      node1 = Objects.requireNonNull( BasicEagerDependencyModel_Sting_MyModel0.create() );
-    }
-    assert null != node1;
-    return node1;
   }
 
   @Nonnull
   private Object node2() {
     if ( null == node2 ) {
-      node2 = Objects.requireNonNull( BasicEagerDependencyModel_Sting_MyModel3.create(node1(), node3) );
+      node2 = Objects.requireNonNull( BasicEagerDependencyModel_Sting_MyModel0.create() );
     }
     assert null != node2;
     return node2;
+  }
+
+  @Nonnull
+  private Object node3() {
+    if ( null == node3 ) {
+      node3 = Objects.requireNonNull( BasicEagerDependencyModel_Sting_MyModel3.create(node2(), node1) );
+    }
+    assert null != node3;
+    return node3;
   }
 
   @Override
